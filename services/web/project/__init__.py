0c6dbc362c339f67bf874338e4c6fa531a2f5a27
Bennie Robinson <bennrobi@cisco.com>
2021-05-19 11:33:05 -0400

    Base repo

---
+++ services/web/project/__init__.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request
+from flask.helpers import send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+import os
+
+
+app = Flask(__name__)
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
+
+
+@app.route("/")
+def hello_world():
+    return jsonify(hello="world")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
+
+
+@app.route("/media/<path:filename>")
+def mediafiles(filename):
+    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Upload New File</title>
+    </head>
+    <body>
+        <form action="" method="post" enctype="multipart/form-data">
+            <p>
+                <input type="file" name="file" />
+                <input type="submit" value="Upload" />
+            </p>
+        </form>
+    </body>
+    </html>
+    """
